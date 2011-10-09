ded7d9d2823377e0928dafd14a9973a07da7758c
brutalville@gmail.com <brutalville@gmail.com@c06b29ab-115f-81e1-2ce3-d881d2a952c5>
2011-10-09 02:55:02 +0000

    Tagged for v1.3

---
+++ branches/v1.3/ClientCode/buildcraft/zeldo/MutiPlayerProxy.java
@@ -0,0 +1,131 @@
+package net.minecraft.src.buildcraft.zeldo;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import net.minecraft.src.IInventory;
+import net.minecraft.src.ModLoader;
+import net.minecraft.src.mod_AdditionalPipes;
+import net.minecraft.src.mod_AdditionalPipes.chunkXZ;
+import net.minecraft.src.buildcraft.api.APIProxy;
+import net.minecraft.src.buildcraft.transport.TileGenericPipe;
+import net.minecraft.src.buildcraft.zeldo.gui.GuiAdvancedWoodPipe;
+import net.minecraft.src.buildcraft.zeldo.gui.GuiItemTeleportPipe;
+import net.minecraft.src.buildcraft.zeldo.gui.GuiLiquidTeleportPipe;
+import net.minecraft.src.buildcraft.zeldo.gui.GuiPowerTeleportPipe;
+
+public class MutiPlayerProxy {
+	public static boolean NeedsLoad = true;
+	public static File WorldDir;
+	public static boolean isServer = false;
+	public static void displayGUIItemTeleport(TileGenericPipe tilePipe) {
+		if (!APIProxy.isClient(APIProxy.getWorld())) {
+			ModLoader.getMinecraftInstance().displayGuiScreen(new GuiItemTeleportPipe(tilePipe));
+		}
+	}
+	public static void displayGUILiquidTeleport(TileGenericPipe tilePipe) {
+		if (!APIProxy.isClient(APIProxy.getWorld())) {
+			ModLoader.getMinecraftInstance().displayGuiScreen(new GuiLiquidTeleportPipe(tilePipe));
+		}
+	}
+	public static void displayGUIPowerTeleport(TileGenericPipe tilePipe) {
+		if (!APIProxy.isClient(APIProxy.getWorld())) {
+			ModLoader.getMinecraftInstance().displayGuiScreen(new GuiPowerTeleportPipe(tilePipe));
+		}
+	}
+	public static void displayGUIAdvancedWood(IInventory PlayerInv, TileGenericPipe container) {
+		if (!APIProxy.isClient(APIProxy.getWorld())) {
+			ModLoader.getMinecraftInstance().displayGuiScreen(new GuiAdvancedWoodPipe(PlayerInv, container, container));
+		}
+	}
+	public static boolean isOnServer()
+	{
+		return false;
+	}
+	public static void AddChunkToList(int x, int z) {
+		if (isOnServer())
+			return;
+		MutiPlayerProxy.LoadChunkData();
+		x = x >> 4;
+		z = z >> 4;
+		Iterator<chunkXZ> chunks = mod_AdditionalPipes.keepLoadedChunks.iterator();
+		while (chunks.hasNext()) {
+			chunkXZ curChunk = (chunkXZ)chunks.next();
+			if (curChunk.x == x && curChunk.z == z) {
+				//System.out.println("Didn't need to add PermChunk @ " + x + "," + z);
+				return;
+			}
+				
+		}
+		mod_AdditionalPipes.keepLoadedChunks.add(new mod_AdditionalPipes.chunkXZ(x, z));
+		 //System.out.println("Added PermChunk @ " + x + "," + z);
+		 SaveChunkData();
+	}
+	public static void SaveChunkData() {
+		try {
+			
+			//System.out.println("Saving ChunkLoader data...");
+	        FileOutputStream fos = new FileOutputStream(getChunkSaveFile().getAbsolutePath());
+	        GZIPOutputStream gzos = new GZIPOutputStream(fos);
+	        ObjectOutputStream out = new ObjectOutputStream(gzos);
+	        out.writeObject(mod_AdditionalPipes.keepLoadedChunks);
+	        out.flush();
+	        out.close();
+	        //System.out.println("Saved ChunkLoader data...");
+	     }
+	     catch (IOException e) {
+	    	 e.printStackTrace(); 
+	     }
+	}
+	@SuppressWarnings("unchecked")
+	public static void LoadChunkData() {
+		if (!NeedsLoad)
+			return;
+		NeedsLoad = false;
+		 try {
+		        FileInputStream fis = new FileInputStream(getChunkSaveFile().getAbsolutePath());
+		        GZIPInputStream gzis = new GZIPInputStream(fis);
+		        ObjectInputStream in = new ObjectInputStream(gzis);
+		        List<mod_AdditionalPipes.chunkXZ> loaded = (List<mod_AdditionalPipes.chunkXZ>)in.readObject();
+		        in.close();
+		        mod_AdditionalPipes.keepLoadedChunks = loaded;
+		        System.out.println("Loaded " + loaded.size() + " Forced Chunks");
+		      }
+		      catch (Exception e) {
+		          e.printStackTrace();
+		      }
+	}
+	public static void DeleteChunkFromList(int x, int z) {
+		if (isOnServer())
+			return;
+		MutiPlayerProxy.LoadChunkData();
+		x = x >> 4;
+		z = z >> 4;
+		Iterator<chunkXZ> chunks = mod_AdditionalPipes.keepLoadedChunks.iterator();
+		while (chunks.hasNext()) {
+			chunkXZ curChunk = (chunkXZ)chunks.next();
+			if (curChunk.x == x && curChunk.z == z) {
+				mod_AdditionalPipes.keepLoadedChunks.remove(curChunk);
+				//System.out.println("Removed PermChunk @ " + x + "," + z);
+				SaveChunkData();
+				return;
+			}
+				
+		}
+	}
+	public static File getChunkSaveFile() {
+		//With MC being dumb we have to load the world location ourselfs
+		if (WorldDir == null)
+			WorldDir = mod_AdditionalPipes.getSaveDirectory();
+		return new File(WorldDir, "ChunkLoader.doNotTouch");
+	}
+	
+}
---
+++ branches/v1.3/ClientCode/buildcraft/zeldo/pipes/PipeItemsDistributor.java
@@ -0,0 +1,70 @@
+/** 
+ * BuildCraft is open-source. It is distributed under the terms of the 
+ * BuildCraft Open Source License. It grants rights to read, modify, compile
+ * or run the code. It does *NOT* grant the right to redistribute this software
+ * or its modifications in any form, binary or source, except if expressively
+ * granted by the copyright holder.
+ */
+
+package net.minecraft.src.buildcraft.zeldo.pipes;
+
+import java.util.LinkedList;
+
+import net.minecraft.src.mod_AdditionalPipes;
+import net.minecraft.src.buildcraft.api.EntityPassiveItem;
+import net.minecraft.src.buildcraft.api.Orientations;
+import net.minecraft.src.buildcraft.api.Position;
+import net.minecraft.src.buildcraft.transport.IPipeTransportItemsHook;
+import net.minecraft.src.buildcraft.transport.Pipe;
+import net.minecraft.src.buildcraft.transport.PipeTransportItems;
+import net.minecraft.src.buildcraft.zeldo.logic.PipeLogicDistributor;
+
+public class PipeItemsDistributor extends Pipe implements IPipeTransportItemsHook {
+	
+	private int baseTexture = mod_AdditionalPipes.DEFUALT_DISTRIBUTOR_TEXTURE;
+	private int plainTexture = mod_AdditionalPipes.DEFUALT_DISTRIBUTOR_TEXTURE_CLOSED;
+	private int nextTexture = baseTexture;
+
+	public PipeItemsDistributor(int itemID) {
+		super(new PipeTransportItems(), new PipeLogicDistributor(), itemID);
+	}
+	
+	@Override
+	public void prepareTextureFor(Orientations connection) {
+		if (connection == Orientations.Unknown) {
+			nextTexture = baseTexture;
+		} else {
+			int metadata = worldObj.getBlockMetadata(xCoord, yCoord, zCoord);
+
+			if (metadata == connection.ordinal()) {
+				nextTexture = baseTexture;
+			} else {
+				nextTexture = plainTexture;
+			}
+		}
+	}
+	
+	@Override
+	public int getBlockTexture() {
+		return nextTexture;
+	}
+
+	@Override
+	public LinkedList<Orientations> filterPossibleMovements(
+			LinkedList<Orientations> possibleOrientations, Position pos,
+			EntityPassiveItem item) {
+		((PipeLogicDistributor)this.logic).switchPosition();
+		worldObj.markBlockNeedsUpdate(xCoord, yCoord, zCoord);
+		return possibleOrientations;
+	}
+
+	@Override
+	public void entityEntered(EntityPassiveItem item, Orientations orientation) {	
+		
+	}
+
+	@Override
+	public void readjustSpeed(EntityPassiveItem item) {
+	}
+
+}
